d90867ab66af9d209ecbd0391d777e757c041d50
Ricardo Chairez <60669495+RicardoChairez@users.noreply.github.com>
2022-04-14 11:22:02 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,122 @@
+var instrumentsLength = document.querySelectorAll(".key").length;
+var clickDown = false;
+var lastKey;
+var lastNote;
+var keyArray = [];
+var noteDisplay = document.querySelector("#note");
+
+//non html key object
+function key(note, keyboardKey, keyObject){
+  this.note = note;
+  this.keyboardKey = keyboardKey;
+  this.keyObject = keyObject;
+  this.play = false;
+  this.audio = new Audio("piano_sounds/" + this.note + ".mp3");
+
+  //make functions for switching key on or off
+}
+
+//gets key in keyArray with keyboardKey as index
+function getKey(key){
+  for(var i = 0; i < instrumentsLength; i++){
+    if(key == keyArray[i].keyboardKey){
+      return keyArray[i];
+    }
+  }
+}
+
+//gets current note being played
+function displayChord(){
+  var noteString = "";
+  for(var i = 0; i < instrumentsLength; i++){
+    if(keyArray[i].play){
+      noteString = noteString + keyArray[i].note + " ";
+    }
+  }
+  noteString = noteString.replace(/[0-9]/g, '');
+  noteDisplay.innerHTML = noteString;
+}
+
+//takes in keyobject and determines if it is a black or white key
+function isBlack(keyObject){
+  if(keyObject.className.split(" ")[2] == "black"){
+    return true;
+  }
+  return false;
+}
+
+//plays a sound given the key
+function playSound(key){
+  var key_audio = new Audio("piano-sounds/" + key.note + ".mp3");
+  key_audio.play();
+}
+
+//stops sound given the key
+function stopSound(key){
+  //var key_audio = new Audio("piano_sounds/" + key + ".mp3");
+  key.audio.pause();
+  key.audio.currentTime = 0;
+}
+
+/*
+function playSoundKey(key){
+  switch(key){
+    case "a":
+      playSound("c1");
+      break;
+    case "w":
+      playSound("db1");
+      break;
+    case "s":
+      playSound("d1");
+      break;
+    case "e":
+      playSound("eb1");
+      break;
+    case "d":
+      playSound("e1");
+      break;
+    case "f":
+      playSound("f1");
+      break;
+    case "t":
+      playSound("gb1");
+      break;
+    case "g":
+      playSound("g1");
+      break;
+    case "y":
+      playSound("ab1");
+      break;
+    case "h":
+      playSound("a1");
+      break;
+    case "u":
+      playSound("bb1");
+      break;
+    case "j":
+      playSound("b1");
+      break;
+    case "k":
+      playSound("c2");
+      break;
+    case "o":
+      playSound("db2");
+      break;
+    case "l":
+      playSound("d2");
+      break;
+    case "p":
+      playSound("eb2");
+      break;
+    case ";":
+      playSound("e2");
+      break;
+    case "'":
+      playSound("f2");
+      break;
+    default:
+      console.log(key);
+  }
+}
+*/
---
+++ index2.js
@@ -0,0 +1,110 @@
+//initializes key objects for each piano key
+for(var i = 0; i < instrumentsLength; i++){
+  keyObject = document.querySelectorAll(".key")[i];
+  keyArray[i] = new key(keyObject.className.split(" ")[0], keyObject.innerHTML, keyObject);
+}
+
+//checks for keyup
+document.addEventListener("keydown", function(){
+  if(lastKey != event.key){
+    lastKey = event.key;
+    key = getKey(lastKey);
+    if(key){
+      key.keyObject.style.backgroundColor = "#FFCBCB";
+      key.play = true;
+      playSound(key);
+      displayChord();
+    }
+  }
+});
+
+//checks for keyup
+document.addEventListener("keyup", function(){
+  //keyObject = document.querySelector("." + event.key);
+  key = getKey(event.key);
+  if(key){
+    key.play = false;
+    displayChord();
+    if(isBlack(key.keyObject)){
+      key.keyObject.style.backgroundColor = "black";
+    }
+    else{
+      key.keyObject.style.backgroundColor = "white";
+    }
+    if(event.key == lastKey){
+      lastKey = null;
+    }
+  }
+});
+
+//listens for mouse click
+for(var i=0; i < instrumentsLength; i++){
+  document.querySelectorAll(".key")[i].addEventListener("mousedown", function(){
+
+      lastNote = this;
+      var key = getKey(this.innerHTML);
+      if(key){
+        playSound(key);
+        key.keyObject.style.backgroundColor = "#FFCBCB";
+        key.play = true;
+        displayChord();
+      }
+
+
+  });
+}
+
+//listens for mouse held and over
+for(var i=0; i < instrumentsLength; i++){
+  document.querySelectorAll(".key")[i].addEventListener("mouseover", function(){
+
+    if(clickDown){
+      lastNote = this;
+      var key = getKey(this.innerHTML);
+      if(key){
+        playSound(key);
+        key.keyObject.style.backgroundColor = "#FFCBCB";
+        key.play = true;
+        displayChord();
+      }
+
+    }
+  });
+}
+
+for(var i=0; i < instrumentsLength; i++){
+  document.querySelectorAll(".key")[i].addEventListener("mouseout", function(){
+    lastNote.play = false;
+    displayChord();
+    if(isBlack(lastNote)){
+      lastNote.style.backgroundColor = "black";
+    }
+    else{
+      lastNote.style.backgroundColor = "white";
+    }
+  });
+}
+
+
+document.querySelector("body").addEventListener("mousedown", function(){
+
+  clickDown = true;
+
+});
+
+document.querySelector("body").addEventListener("mouseup", function(){
+
+  clickDown = false;
+  for(var i = 0; i < instrumentsLength; i++){
+    keyArray[i].play = false;
+  }
+  displayChord();
+  if(isBlack(lastNote)){
+    lastNote.style.backgroundColor = "black";
+  }
+  else{
+    lastNote.style.backgroundColor = "white";
+  }
+
+
+});
